3ed0263ef4af5c77428ec731de9b78a761144281
Alexander Nitiola <anitiola@aellacredit.com>
2023-04-15 06:35:18 +0100

    Created the submit button

--- app/src/main/kotlin/com/example/kotlinreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/kotlin/com/example/kotlinreader/screens/login/ReaderLoginScreen.kt
@@ -4,10 +4,14 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -78,6 +82,33 @@ fun UserForm(
                 onDone(email.value.trim(), password.value.trim())
             }
         )
+        SubmitButton(
+            textId = if (isCreateAccount) "Create Account" else "Login",
+            loading = loading,
+            validInputs = valid,
+        ) {
+            onDone(email.value.trim(), password.value.trim())
+        }
+    }
+}
+
+@Composable
+fun SubmitButton(
+    textId: String,
+    loading: Boolean,
+    validInputs: Boolean,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .padding(start = 24.dp, end = 24.dp)
+            .fillMaxWidth(),
+        onClick = onClick,
+        enabled = !loading && validInputs,
+        shape = CircleShape
+    ) {
+        if (loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        else Text(text = textId, modifier = Modifier.padding(5.dp))
     }
 }
 
