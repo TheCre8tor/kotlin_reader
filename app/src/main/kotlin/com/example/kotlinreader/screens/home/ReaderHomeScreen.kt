1bc19b3e0cd59a9eeafa934b528942f803979e52
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-04-18 19:04:55 +0100

    Created a FAB Button

--- app/src/main/kotlin/com/example/kotlinreader/screens/home/ReaderHomeScreen.kt
+++ app/src/main/kotlin/com/example/kotlinreader/screens/home/ReaderHomeScreen.kt
@@ -1,10 +1,39 @@
 package com.example.kotlinreader.screens.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderHomeScreen(navController: NavController) {
-    Text(text = "Home Page")
+    Scaffold(
+        topBar = {},
+        floatingActionButton = {
+            FABContent {}
+        }
+    ) { paddingValues ->
+
+    }
+}
+
+@Composable
+fun FABContent(onTap: (String) -> Unit) {
+    FloatingActionButton(
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = MaterialTheme.colors.background,
+        onClick = { /*TODO*/ }
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a Book",
+            tint = MaterialTheme.colors.onSecondary
+        )
+    }
 }
